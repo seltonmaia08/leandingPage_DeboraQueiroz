b16e6646d0f2a8dd19762f373d3b87479e489ba9
seltonmaia08 <seltonmaia08@gmail.com>
2025-12-24 18:38:49 -0300

    Add fontes, section apresentation e dropdown

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
 // Adcionar background especial nas divs Relatos alternadamente
-document.querySelectorAll('.relatos')
-    .forEach((relato, index) => {
-        if (index % 2 === 0) {
-            relato.classList.add('bg_alternativo')
-        }
-    })
+// document.querySelectorAll('.relatos')
+//     .forEach((relato, index) => {
+//         if (index % 2 === 0) {
+//             relato.classList.add('bg_alternativo')
+//         }
+//     })
 
-document.querySelector('body').animate
+const dropdown = document.querySelector('.dropdown');
+const btn = document.querySelector('.dropdown-btn');
+
+btn.addEventListener('click', () => {
+    dropdown.classList.toggle('active');
+});
+
+// fecha ao clicar fora
+document.addEventListener('click', (e) => {
+    if (!dropdown.contains(e.target)) {
+        dropdown.classList.remove('active');
+    }
+});
